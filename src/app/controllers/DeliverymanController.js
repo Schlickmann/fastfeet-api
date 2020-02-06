fca28c0c2005523bb377947e59b7ef2c246d8021
Juliani Schlickmann Damasceno <juliani.schlickmann@gmail.com>
2020-02-05 19:03:37 -0800

    added new migrations to create files table and avatar_id in users table

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,26 @@
+import * as Yup from 'yup';
+import User from '../models/User';
+
+class DeliverymanController {
+  async index(req, res) {
+    return res.json();
+  }
+
+  async store(req, res) {
+    return res.json();
+  }
+
+  async show(req, res) {
+    return res.json();
+  }
+
+  async update(req, res) {
+    return res.json();
+  }
+
+  async delete(req, res) {
+    return res.json();
+  }
+}
+
+export default new DeliverymanController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UserAdministratorController from './app/controllers/UserAdministratorController';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -27,5 +28,12 @@ routes.get('/recipients/:id', RecipientController.show);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.delete);
+
+// Deliveryman
+routes.get('/couriers', DeliverymanController.index);
+routes.get('/couriers/:id', DeliverymanController.show);
+routes.post('/couriers', DeliverymanController.store);
+routes.put('/couriers/:id', DeliverymanController.update);
+routes.delete('/couriers/:id', DeliverymanController.delete);
 
 export default routes;
